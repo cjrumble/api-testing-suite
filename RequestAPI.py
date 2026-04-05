f7ad59379df1bd6073a7f5f302b453cca4af6a0e
cjrumble <cjrumble@users.noreply.github.com>
2026-04-05 15:54:49 -0700

    Document REST API HTTP response codes
    Added comments explaining REST API HTTP response codes.

--- RequestAPI.py
+++ RequestAPI.py
@@ -1,3 +1,4 @@
+# https://github.com/naveenanimation20/APITesting_Python_RequestModule/blob/master/GoRestAPI/RequestAPI.py
 import requests
 import random
 import json
@@ -95,3 +96,19 @@ get_request()
 user_id = post_request()
 put_request(user_id)
 delete_request(user_id)
+
+
+# REST API Http Response Codes
+# 200: OK. Everything worked as expected.
+# 201: A resource was successfully created in response to a POST request. The Location header contains the URL pointing to the newly created resource.
+# 204: The request was handled successfully and the response contains no body content (like a DELETE request).
+# 304: The resource was not modified. You can use the cached version.
+# 400: Bad request. This could be caused by various actions by the user, such as providing invalid JSON data in the request body etc.
+# 401: Authentication failed.
+# 403: The authenticated user is not allowed to access the specified API endpoint.
+# 404: The requested resource does not exist.
+# 405: Method not allowed. Please check the Allow header for the allowed HTTP methods.
+# 415: Unsupported media type. The requested content type or version number is invalid.
+# 422: Data validation failed (in response to a POST request, for example). Please check the response body for detailed error messages.
+# 429: Too many requests. The request was rejected due to rate limiting.
+# 500: Internal server error. This could be caused by internal program errors.
